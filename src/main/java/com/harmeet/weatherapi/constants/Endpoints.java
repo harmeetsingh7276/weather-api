08de060c709213d4744d7228b79abd6bffa3c2ad
harmeetsinghgorowada <harmeetsingh7276@gmail.com>
2023-12-29 15:37:07 +0530

    indentation

--- src/main/java/com/harmeet/weatherapi/constants/Endpoints.java
+++ src/main/java/com/harmeet/weatherapi/constants/Endpoints.java
@@ -6,8 +6,5 @@ import lombok.Data;
 public class Endpoints {
     public static final String BASE_URL = "https://forecast9.p.rapidapi.com/rapidapi";
     public static final String RAPID_API_GET_FORECAST_SUMMARY_BY_LOCATION_NAME = BASE_URL + "/forecast/%s/summary/";
-
-
     public static final String RAPID_API_GET_HOURLY_FORECAST_BY_LOCATION_NAME = BASE_URL + "/forecast/%s/hourly/";
-    ;
 }
